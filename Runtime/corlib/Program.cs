652308a3a9c187f6142037cb56b375bae02fb3c6
Orvid King <blah38621@gmail.com>
2012-03-03 13:12:00 -0600

    Fixing Astaelan's mess.

--- Runtime/corlib/Program.cs
+++ Runtime/corlib/Program.cs
@@ -7,12 +7,14 @@ namespace Mernel
     {
         private static void Main()
         {
-            Console.WriteLine("Mernel: Startup");
-            string str = "Are you sure?";
-            string str2 = "Definately.";
+            uint a = 15;
+            Console.Write(test());
+            //Console.WriteLine("Mernel: Startup");
+            //string str = "Are you sure?";
+            //string str2 = "Definately.";
             ////str2 = str2.Replace("fin", "fan");
             ////str2 = str2.Replace("Definately", "Fosho!");
-            Console.WriteLine(str + " " + str2);
+            //Console.WriteLine(str + "\n" + str2);
             //int a = 2;
             //int b = 78;
             //b = 2;
@@ -24,12 +26,18 @@ namespace Mernel
             //    Console.WriteLine("It Isn't!");
             //string st4;
             //string str3 = ((uint)1).ToString();
-            Console.WriteLine((string)((uint)1).ToString());
+            Console.WriteLine(a.ToString());
+            Console.WriteLine("Factorial of 6 (Via Addition): " + Factorial(6).ToString());
         }
 
-        private static string Factorial(int val, string str)
+        private static string test()
         {
-            return val == 0 ? str : str + Factorial(val - 1, str + " a");
+            return "Test\n";
+        }
+		
+        private static uint Factorial(uint val)
+        {
+            return val == 0 ? val : val + Factorial(val - 1);
         }
     }
 }
