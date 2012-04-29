a8471e9cc74c56da17b8cfced4139272c35bb5fe
Orvid King <blah38621@gmail.com>
2012-04-29 10:09:07 -0500

    Switched branch processing over to use a switch statement rather than if/else.

--- Nernel/Source/CLR/IROptimizer.c
+++ Nernel/Source/CLR/IROptimizer.c
@@ -30,36 +30,45 @@ uint32_t IROptimizer_ProcessBranches(IRMethod* pMethod, uint32_t pStartIndex, IR
 	while (index < pMethod->IRCodesCount)
 	{
 		instruction = pMethod->IRCodes[index];
-		if (instruction->Opcode == IROpcode_Return) return index;
-		else if (instruction->Opcode == IROpcode_Branch)
+		switch(instruction->Opcode)
 		{
-			if ((BranchCondition)(uint32_t)instruction->Arg1 == BranchCondition_Always) return ((IRInstruction*)instruction->Arg2)->IRLocation;
-			else
+			case IROpcode_Return:
+			case IROpcode_Throw:
+				return index;
+			case IROpcode_Branch:
 			{
-				bool_t alreadyAdded = FALSE;
-				for (uint32_t searchIndex = 0; searchIndex < *pBranchesCount; ++searchIndex)
+				if ((BranchCondition)(uint32_t)instruction->Arg1 == BranchCondition_Always) return ((IRInstruction*)instruction->Arg2)->IRLocation;
+				else
 				{
-					if ((*pBranches)[searchIndex].FirstLeftInstruction == index + 1 &&
-						(*pBranches)[searchIndex].FirstRightInstruction == ((IRInstruction*)instruction->Arg2)->IRLocation)
+					bool_t alreadyAdded = FALSE;
+					for (uint32_t searchIndex = 0; searchIndex < *pBranchesCount; ++searchIndex)
 					{
-						alreadyAdded = TRUE;
-						break;
+						if ((*pBranches)[searchIndex].FirstLeftInstruction == index + 1 &&
+							(*pBranches)[searchIndex].FirstRightInstruction == ((IRInstruction*)instruction->Arg2)->IRLocation)
+						{
+							alreadyAdded = TRUE;
+							break;
+						}
+					}
+					if (!alreadyAdded)
+					{
+						uint32_t branchIndex = *pBranchesCount;
+						*pBranchesCount += 1;
+						*pBranches = (IRBranch*)realloc(*pBranches, *pBranchesCount * sizeof(IRBranch));
+						(*pBranches)[branchIndex].FirstLeftInstruction = index + 1;
+						(*pBranches)[branchIndex].FirstRightInstruction = ((IRInstruction*)instruction->Arg2)->IRLocation;
+						(*pBranches)[branchIndex].LeftConvergence = IROptimizer_ProcessBranches(pMethod, index + 1, pBranches, pBranchesCount);
+						(*pBranches)[branchIndex].RightConvergence = IROptimizer_ProcessBranches(pMethod, ((IRInstruction*)instruction->Arg2)->IRLocation, pBranches, pBranchesCount);
 					}
 				}
-				if (!alreadyAdded)
-				{
-					uint32_t branchIndex = *pBranchesCount;
-					*pBranchesCount += 1;
-					*pBranches = (IRBranch*)realloc(*pBranches, *pBranchesCount * sizeof(IRBranch));
-					(*pBranches)[branchIndex].FirstLeftInstruction = index + 1;
-					(*pBranches)[branchIndex].FirstRightInstruction = ((IRInstruction*)instruction->Arg2)->IRLocation;
-					(*pBranches)[branchIndex].LeftConvergence = IROptimizer_ProcessBranches(pMethod, index + 1, pBranches, pBranchesCount);
-					(*pBranches)[branchIndex].RightConvergence = IROptimizer_ProcessBranches(pMethod, ((IRInstruction*)instruction->Arg2)->IRLocation, pBranches, pBranchesCount);
-				}
 			}
+			case IROpcode_Jump:
+				Panic("Don't know how to handle this yet!");
+				break;
+
+			default:
+				break;
 		}
-		else if (instruction->Opcode == IROpcode_Jump) Panic("Don't know how to handle this yet!");
-		// Handle IROpcode_Throw as well, but not yet because we have throws in corlib
 		++index;
 	}
 	return index;
