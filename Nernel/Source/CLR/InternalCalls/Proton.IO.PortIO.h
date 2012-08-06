27daf444c48db49a0f0893e4ffabbcf384c80be7
Shane Bryldt <astaelan@gmail.com>
2012-08-06 02:24:16 -0600

    Small fix to the PortIO internal calls, as all internal calls must pass appdomain pointer AND stack stream pointer as first 2 parameters

--- Nernel/Source/CLR/InternalCalls/Proton.IO.PortIO.h
+++ Nernel/Source/CLR/InternalCalls/Proton.IO.PortIO.h
@@ -2,10 +2,10 @@
 
 #include <CLR/AppDomain.h>
 
-uint32_t Proton_IO_PortIO_InByte(AppDomain* pAppDomain, uint32_t port);
-uint32_t Proton_IO_PortIO_InUShort(AppDomain* pAppDomain, uint32_t port);
-uint32_t Proton_IO_PortIO_InUInt(AppDomain* pAppDomain, uint32_t port);
+uint32_t Proton_IO_PortIO_InByte(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port);
+uint32_t Proton_IO_PortIO_InUShort(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port);
+uint32_t Proton_IO_PortIO_InUInt(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port);
 
-void Proton_IO_PortIO_OutByte(AppDomain* pAppDomain, uint32_t port, uint32_t value);
-void Proton_IO_PortIO_OutUShort(AppDomain* pAppDomain, uint32_t port, uint32_t value);
-void Proton_IO_PortIO_OutUInt(AppDomain* pAppDomain, uint32_t port, uint32_t value);
+void Proton_IO_PortIO_OutByte(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port, uint32_t value);
+void Proton_IO_PortIO_OutUShort(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port, uint32_t value);
+void Proton_IO_PortIO_OutUInt(AppDomain* pAppDomain, uint8_t* pStackStream, uint32_t port, uint32_t value);
