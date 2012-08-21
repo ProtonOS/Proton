c80f226ce0705d882f7cd02ae5a6b390f5529de5
Orvid King <blah38621@gmail.com>
2012-08-21 15:12:58 -0500

    Custom attribute FixedArgs work for everything except boxed objects.

--- OS/Mernel/Kernel.cs
+++ OS/Mernel/Kernel.cs
@@ -4,8 +4,34 @@ using System.Runtime.CompilerServices;
 
 namespace Mernel
 {
+
+    public enum TestEnum
+    {
+        TestValue1,
+        TestValue2,
+    }
+
+    [Test(1, 2, 3, 4, 5, 6, 7, 8, "Hello World", typeof(Kernel), TestEnum.TestValue1, new int[] { 0, 1, 2, 3, 4 }, new Type[] { typeof(AttributeTest), typeof(Kernel) }, new TestEnum[] { TestEnum.TestValue1, TestEnum.TestValue2 })]
+    public class AttributeTest
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class TestAttribute : Attribute
+    {
+        public TestAttribute(byte pByteVal, sbyte pSByteVal, ushort pUShortVal, short pShortVal, uint pUIntVal, int pIntVal, ulong pULongVal, long pLongVal, string pStringVal, Type pTypeVal, TestEnum pEnumVal, int[] pIntArrayVal, Type[] pTypeArrayVal, TestEnum[] pEnumArrayVal)
+        {
+
+        }
+    }
     public static class Kernel
     {
+        public static readonly byte[] Values = new byte[]
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+        };
+
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern void Write(string pString);
 
