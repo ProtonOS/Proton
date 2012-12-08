8be8dd11226c30c4557b7e95072a09165878f046
Shane Bryldt <astaelan@gmail.com>
2012-12-08 15:24:43 -0700

    Created Proton.SDKInstaller project, this can be used for first time users to obtain the SDK, and to update the SDK as it changes, supporting multiple host and targets, currently only x86 is available for both

---
+++ Proton.SDKInstaller/SDKPackages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proton.SDKInstaller
+{
+    public sealed class SDKPackages
+    {
+        public sealed class Package
+        {
+            public string Name = "";
+            public DateTime Updated = DateTime.UtcNow;
+        }
+
+		public List<Package> HostPackages = new List<Package>();
+		public List<Package> TargetPackages = new List<Package>();
+	}
+}
