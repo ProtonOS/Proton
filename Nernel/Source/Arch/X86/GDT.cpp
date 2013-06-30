851dcc42ddff3faeab09bdc29c69351f124a4109
Shane Bryldt <astaelan@gmail.com>
2013-06-30 07:42:20 -0600

    Added some GDT related stuff

--- Nernel/Source/Arch/X86/GDT.cpp
+++ Nernel/Source/Arch/X86/GDT.cpp
@@ -1,4 +1,4 @@
-#include "../../PrimitiveTypes.h"
+#include "GDT.h"
 
 struct GDTRegister
 {
@@ -50,8 +50,55 @@ public:
    uint16 iomap_base;
 };
 
+static const uint32 GDTDescriptorCount = 5;
 
 // These will be assigned by Boot.asm
 GDTRegister* gGDTRegister = nullptr;
 GDTDescriptor* gGDTDescriptors = nullptr;
+uint32 gGDTDescriptorMax = 0;
+uint32 gGDTDescriptorCount = 0;
 
+// 512 comes from the number of GDTDescriptors that can be stored within 4096 bytes at 0xA000
+// would be nice to get this out of Boot.asm, but it would not be constant, which is required
+// for the gTSSDescriptors static allocation
+static const uint32 TSSDescriptorMax = 512 - GDTDescriptorCount;
+
+TSSDescriptor gTSSDescriptors[TSSDescriptorMax];
+
+
+void GDT::SetSegment(uint32 pIndex, uint32 pAddress, uint32 pLimit, uint8 pAccess, uint8 pFlags)
+{
+    gGDTDescriptors[pIndex].AddressLow = pAddress & 0xFFFF;
+    gGDTDescriptors[pIndex].AddressMid = (pAddress >> 16) & 0xFF;
+    gGDTDescriptors[pIndex].AddressHigh = (pAddress >> 24) & 0xFF;
+    gGDTDescriptors[pIndex].LimitLow = pLimit & 0xFFFF;
+    gGDTDescriptors[pIndex].LimitHighAndFlags = ((pLimit >> 16) & 0x0F) | (pFlags & 0xF0);
+    gGDTDescriptors[pIndex].Access = pAccess;
+}
+
+void GDT::Load()
+{
+    gGDTRegister->Limit = (sizeof(GDTDescriptor) * gGDTDescriptorMax) - 1;
+    gGDTRegister->Address = (uintptr)&gGDTDescriptors[0];
+
+	for (uint32 gdtDescriptorIndex = 0; gdtDescriptorIndex < gGDTDescriptorMax; ++gdtDescriptorIndex) SetSegment(0, 0x00000000, 0x00000000, 0x00, 0x00);
+
+    SetSegment(1, 0x00000000, 0xFFFFFFFF, AccessReadWriteOnePresent | AccessExecutable, FlagsSelector32BitGranularity4KB);
+    SetSegment(2, 0x00000000, 0xFFFFFFFF, AccessReadWriteOnePresent, FlagsSelector32BitGranularity4KB);
+    SetSegment(3, 0x00000000, 0xFFFFFFFF, AccessReadWriteOnePresent | AccessExecutable | AccessRing3, FlagsSelector32BitGranularity4KB);
+    SetSegment(4, 0x00000000, 0xFFFFFFFF, AccessReadWriteOnePresent | AccessRing3, FlagsSelector32BitGranularity4KB);
+
+	for (uint32 gdtDescriptorIndex = 5, tssDescriptorIndex = 0; gdtDescriptorIndex < gGDTDescriptorMax; ++gdtDescriptorIndex, ++tssDescriptorIndex)
+	{
+		uintptr baseAddress = (uintptr)&gTSSDescriptors[tssDescriptorIndex];
+	    SetSegment(gdtDescriptorIndex, baseAddress, baseAddress + sizeof(TSSDescriptor), 0xE9, 0x00);
+		gTSSDescriptors[tssDescriptorIndex].ss0 = 0x10;
+	}
+
+	GDTUpdate();
+}
+
+void GDT::SetTSSStack(uint32 pIndex, uintptr pStackAddress)
+{
+	gTSSDescriptors[pIndex].esp0 = pStackAddress;
+}
--- Nernel/Source/Main.cpp
+++ Nernel/Source/Main.cpp
@@ -1,10 +1,12 @@
 #include "MultiBoot.h"
 #include "KernelConsole.h"
+#include "Arch.h"
 
 extern "C" void Main(uint32 pMultiBootMagic, MultiBoot::MultiBootHeader* pMultiBootHeader)
 {
 	KernelConsole::Load();
 	MultiBoot::Load(pMultiBootMagic, pMultiBootHeader);
+	Arch::Load();
 
 	KernelConsole::WriteLine("Hello World!");
 }
