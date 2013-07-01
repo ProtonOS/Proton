49f6be79127ab291f746e7a3d9eece92c4e33bcc
Shane Bryldt <astaelan@gmail.com>
2013-07-01 07:09:19 -0600

    Added a few comments to show the remaining path of code to be implemented before work on the JIT engine can begin

--- Nernel/Source/Main.cpp
+++ Nernel/Source/Main.cpp
@@ -5,10 +5,30 @@
 
 extern "C" void Main(UInt32 pMultiBootMagic, MultiBoot::MultiBootHeader* pMultiBootHeader)
 {
+	// Debugging Support
     KernelConsole::Load();
+
+	// Preliminary Memory Management
     MultiBoot::Load(pMultiBootMagic, pMultiBootHeader);
+
+	// Architecture Specific Support
     Arch::Load();
+
+	// Debugging Support
 	SystemClock::Load();
+
+	// Complete Memory Management
+	// PhysicalMemory::Load();
+	// VirtualMemory::Load();
+
+	// Preemptive MultiTasking Support
+	// ThreadScheduler::Load();
+
+	// MultiProcessor Support
+	// SMP::Load();
+
+	// Bootstrap Processor UserMode Boot
+	// UserModeBoot()
 
     KernelConsole::WriteLine("Hello World!");
     while (true);
