4ee9664fe769888fc856a5961f4a0bf4efd50b80
Shane Bryldt <astaelan@gmail.com>
2013-07-04 13:21:08 -0600

    Added SDKInstaller, currently only supports win32 host, and i686 target (which is also the default when running without arguments)

---
+++ SDKInstaller/SDKPackages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SDKInstaller
+{
+    public sealed class SDKPackages
+    {
+        public sealed class Host
+        {
+            public sealed class Target
+            {
+                public string Name = "";
+                public DateTime Updated = DateTime.UtcNow;
+            }
+
+            public string Name = "";
+            public DateTime Updated = DateTime.UtcNow;
+            public List<Target> Targets = new List<Target>();
+        }
+
+        public List<Host> Hosts = new List<Host>();
+    }
+}
